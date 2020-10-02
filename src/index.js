7c1ef5bb44563bced5091abc587e63c66fa85ddb
Conor Meade <cmeade6479@gmail.com>
2020-10-01 21:28:38 -0400

    moved drop down to individual entry form for each list

--- src/index.js
+++ src/index.js
@@ -6,106 +6,103 @@ function User(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-class CreateEntry extends React.Component{
+class ListEntry extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       text : '',
-      selectedList : "",
-      secondPriority : [],
-      firstPriority : [],
-      finished : []
+      list: []
     };
     this.handleText = this.handleText.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
   }
+  
   handleText(event){
     this.setState({
       text : event.target.value
     });
   }
-  handleSelect(event){
-    this.setState({
-      selectedList : event.target.value
-    });
-    if(this.state.selectedList == "somewhat"){
-      this.setState({
-        secondPriority : [...this.state.secondPriority, this.state.text]
-      });
-    }else if(this.state.selectedList == "very"){
-      this.setState({
-        firstPriority : [...this.state.firstPriority, this.state.text]
-      });
-    }else{
-      this.setState({
-        finished : [...this.state.finished, this.state.text]
-      });
-    }
-  }
 
   handleSubmit(event){
     event.preventDefault();
-    console.log(this.state.secondPriority);
-    console.log(this.state.firstPriority);
-    console.log(this.state.finished);
-    alert('A new task was submitted: ' + this.state.text + ' this list contains the tasks: ' + this.state.selectedList);
+    alert("you submitted " + this.state.text)
+    if (this.state.text.length === 0){
+      return;
+    }
+
+    this.setState({
+      list : this.state.list.concat(this.state.text),
+      text: ''
+    });
+    console.log("List: " + this.state.text.toString())
   }
 
   render(){
     return(
-      <form onSubmit = {this.handleSubmit}>
-        <label>
-          New Entry:
-            <input type="text" value={this.state.text} onChange = {this.handleText} />
-        </label>
-        <label>
-            <select id='sections' name ='sections' onChange={this.handleSelect}>
-              <option value=''></option>
-              <option value={"somewhat"}>Somewhat Important</option>
-              <option value={"very"}>Need to finish</option>
-              <option value={"done"}>Finished</option>
-            </select>
-        </label>
-        <input type ="submit" value="Submit" />
-      </form>
+        <form onSubmit = {this.handleSubmit}>
+          <label>
+            New Entry:
+              <input type="text" value={this.state.text} onChange = {this.handleText} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
     );
   }
 
 }
 
-class List extends React.Component{
 
-  /*Three sections for second most pressing tasks, most pressing tasks, and finished tasks */
+class List extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      secondPriority : [1, 2, 3],
-      firstPriority : [4, 5, 6],
-      finished : [7, 8, 9]
+      list : []
+    };
+    this.updateList = this.updateList.bind(this);
+  }
+
+  updateList(){
+    this.setState({
+      list : this.state.list.concat()
+    });
+  }
+
+  render(){
+    const items = this.props.list.map(i => <li className='entry'>{i}</li>)
+    return(
+      <div>
+        <div>
+          {items}
+        </div>
+        <div className = "entryform">
+          <ListEntry handler = {this.updateList}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ToDo extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      firstPriority : [1],
+      secondPriority : [2],
+      finished : [3]
     };
   }
 
   render(){
-    const second_items = this.state.secondPriority.map(i => <li className='entry'>{i}</li>)
-    const first_items =  this.state.firstPriority.map(i => <li className='entry'>{i}</li>)
-    const finished_items =  this.state.finished.map(i => <li className='entry'>{i}</li>)
     return(
       <div>
         <div className='first'>
-          {/* {this.state.secondPriority} */}
-          {second_items}
+          <List list = {this.state.firstPriority}/>
         </div>
         <div className='second'>
-          {/* {this.state.firstPriority} */}
-          {first_items}
+          <List list = {this.state.secondPriority}/>
         </div>
         <div className='done'>
-          {/* {this.state.finished} */}
-          {finished_items}
-        </div>
-        <div className = "entryform">
-          <CreateEntry />
+          <List list = {this.state.finished}/>
         </div>
       </div>
     );
@@ -168,7 +165,7 @@ class Counter extends React.Component {
 // );
 
 reactDOM.render(
-  <List />,
+  <ToDo />,
   document.getElementById('root')
 );
 
