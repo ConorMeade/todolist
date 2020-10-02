dba244c443a7ecc0d1365bda8fed0a70bd19f0e5
unknown <cmeade6479@gmail.com>
2020-10-02 17:46:00 -0400

    put ListEntry code into ToDo, different lists now update correctly

--- src/index.js
+++ src/index.js
@@ -6,76 +6,16 @@ function User(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
-class ListEntry extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      text : '',
-      list: []
-    };
-    this.handleText = this.handleText.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-  handleText(event){
-    this.setState({
-      text : event.target.value
-    });
-  }
-
-  handleSubmit(event){
-    event.preventDefault();
-    alert("you submitted " + this.state.text)
-    if (this.state.text.length === 0){
-      return;
-    }
-
-    this.setState({
-      list : this.state.list.concat(this.state.text),
-      text: ''
-    });
-    console.log("List: " + this.state.text.toString())
-  }
-
-  render(){
-    return(
-        <form onSubmit = {this.handleSubmit}>
-          <label>
-            New Entry:
-              <input type="text" value={this.state.text} onChange = {this.handleText} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-    );
-  }
-
-}
-
 
 class List extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      list : []
-    };
-    this.updateList = this.updateList.bind(this);
-  }
-
-  updateList(){
-    this.setState({
-      list : this.state.list.concat()
-    });
-  }
-
+  // constructor(props){
+  //   super(props);
+  // }
   render(){
-    const items = this.props.list.map(i => <li className='entry'>{i}</li>)
     return(
       <div>
         <div>
-          {items}
-        </div>
-        <div className = "entryform">
-          <ListEntry handler = {this.updateList}/>
+          {this.props.list.map(i => <li className='entry'>{i}</li>)}
         </div>
       </div>
     );
@@ -86,23 +26,114 @@ class ToDo extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      firstPriority : [1],
-      secondPriority : [2],
-      finished : [3]
+      secondPriority : [],
+      firstPriority : [],
+      finished : [],
+      textSecond : '',
+      textFirst : '',
+      textFinished : ''
     };
+    this.handleTextSecond = this.handleTextSecond.bind(this);
+    this.handleTextFirst = this.handleTextFirst.bind(this);
+    this.handleTextFinished = this.handleTextFinished.bind(this);
+    this.handleSubmitSecond = this.handleSubmitSecond.bind(this);
+    this.handleSubmitFirst = this.handleSubmitFirst.bind(this);
+    this.handleSubmitFinished = this.handleSubmitFinished.bind(this);
+  }
+
+
+  handleTextSecond(event){
+    // event.preventDefault();
+    this.setState({
+      textSecond : event.target.value
+    });
+  }
+
+  handleTextFirst(event){
+    // event.preventDefault();
+    this.setState({
+      textFirst : event.target.value
+    });
+  }
+
+  handleTextFinished(event){
+    // event.preventDefault();
+    this.setState({
+      textFinished : event.target.value
+    });
+  }
+
+  handleSubmitSecond(event){
+    event.preventDefault();
+    alert("you submitted " + this.state.textSecond)
+    if (this.state.textSecond.length === 0){
+      return;
+    }
+
+    this.setState({
+      list : this.state.secondPriority.push(this.state.textSecond),
+      text: ''
+    });
+  }
+
+  handleSubmitFirst(event){
+    event.preventDefault();
+    alert("you submitted " + this.state.textFirst)
+    if (this.state.textFirst.length === 0){
+      return;
+    }
+
+    this.setState({
+      list : this.state.firstPriority.push(this.state.textFirst),
+      text: ''
+    });
+  }
+
+  handleSubmitFinished(event){
+    event.preventDefault();
+    alert("you submitted " + this.state.textFinished)
+    if (this.state.textFinished.length === 0){
+      return;
+    }
+
+    this.setState({
+      list : this.state.finished.push(this.state.textFinished),
+      text: ''
+    });
   }
 
   render(){
     return(
       <div>
-        <div className='first'>
-          <List list = {this.state.firstPriority}/>
-        </div>
         <div className='second'>
           <List list = {this.state.secondPriority}/>
+          <form onSubmit = {this.handleSubmitSecond}>
+          <label>
+            New Entry:
+              <input type="text" value={this.state.textSecond} onChange = {this.handleTextSecond} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </div>
+        <div className='first'>
+          <List list = {this.state.firstPriority}/>
+          <form onSubmit = {this.handleSubmitFirst}>
+          <label>
+            New Entry:
+              <input type="text" value={this.state.textFirst} onChange = {this.handleTextFirst} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         </div>
         <div className='done'>
           <List list = {this.state.finished}/>
+          <form onSubmit = {this.handleSubmitFinished}>
+          <label>
+            New Entry:
+              <input type="text" value={this.state.textFinished} onChange = {this.handleTextFinished} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         </div>
       </div>
     );
@@ -110,53 +141,7 @@ class ToDo extends React.Component{
 }
 
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-    // change code below this line
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-    this.reset = this.reset.bind(this)
-    // change code above this line
-  }
-  // change code below this line
-  increment(){
-    this.setState({
-      count : this.state.count + 1
-    });
-  }
-  decrement(){
-    this.setState({
-      count : this.state.count - 1
-    });
-  }
-  reset(){
-    this.setState(state =>({
-      count : 0
-    }));
-  }
-  // change code above this line
-  render() {
-    return (
-      <div>
-        <button className='inc' onClick={this.increment}>Increment!</button>
-        <button className='dec' onClick={this.decrement}>Decrement!</button>
-        <button className='reset' onClick={this.reset}>Reset</button>
-        <h1>Current Count: {this.state.count}</h1>
-      </div>
-    );
-  }
-};
-
-
-
-
-
-
-  // ========================================
+// ========================================
 
 // const element = <User name = "Conor" />;
 // reactDOM.render(
